d34f7f81ede39ef33cf8475d1b0877f900faadd8
psaicho <guzekt@gmail.com>
2022-10-07 22:55:02 +0200

    82 znaki - jestem na podium

--- chanukah.py
+++ chanukah.py
@@ -1,7 +1,5 @@
+for i in range(int(input())):print(i+1,int((int(input().split()[1])+1.5)**2//2-1))
 
 
 
-for i in range(int(input())):
-    x, y = map(int, input().split())
-    print(x, int((y + 1.5) ** 2 // 2 - 1))
 
